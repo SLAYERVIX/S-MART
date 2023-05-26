73ccb98f2c18ad417588d0d1518b28bf6d10a112
Ahmed <akhaled.vi@gmail.com>
2023-05-26 15:42:56 +0300

    Added login design

--- app/src/main/java/com/example/s_mart/ui/LoginFragment.kt
+++ app/src/main/java/com/example/s_mart/ui/LoginFragment.kt
@@ -1,32 +1,27 @@
-package com.example.s_mart.ui.onboarding
+package com.example.s_mart.ui
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
-import com.example.s_mart.R
-import com.example.s_mart.databinding.FragmentPageThreeBinding
+import com.example.s_mart.databinding.FragmentLoginBinding
 
-class PageThreeFragment : Fragment() {
+class LoginFragment : Fragment() {
 
-    private var _binding: FragmentPageThreeBinding? = null
+    private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentPageThreeBinding.inflate(inflater,container,false)
-
-        binding.button.setOnClickListener {
-            findNavController().navigate(R.id.action_onBoardingFragment_to_loginFragment)
-        }
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
 
         // Inflate the layout for this fragment
         return binding.root
